5b6fc0430439a1e8e916835e2efb4a585d7d2eca
JScarlet <x18321758289@outlook.com>
2020-02-25 01:25:23 +0800

    添加实验代码

--- text_classification_by_fast_bert.py
+++ text_classification_by_fast_bert.py
@@ -9,44 +9,45 @@ from fast_bert.learner_cls import BertLearner
 from definitions import ROOT_DIR
 
 if __name__ == '__main__':
-    data_dir = Path(ROOT_DIR) / 'data'
-    label_dir = Path(ROOT_DIR) / 'data'
-    databunch = BertDataBunch(data_dir, label_dir,
-                              tokenizer='bert-base-uncased',
-                              train_file='train.csv',
-                              val_file='val.csv',
-                              label_file='labels.csv',
-                              text_col='text',
-                              label_col='label',
-                              batch_size_per_gpu=8,
-                              max_seq_length=512,
-                              multi_gpu=True,
-                              multi_label=False,
-                              model_type='bert')
-    logger = logging.getLogger()
-    # device_cuda = torch.device("cuda")
-    device = torch.device('cuda') if torch.cuda.device_count() else torch.device('cpu')
-    metrics = [{'name': 'accuracy', 'function': accuracy}]
+    for i in range(0, 10):
+        data_dir = Path(ROOT_DIR) / ('data' + str(i))
+        label_dir = Path(ROOT_DIR) / ('data' + str(i))
+        databunch = BertDataBunch(data_dir, label_dir,
+                                  tokenizer='bert-base-uncased',
+                                  train_file='train.csv',
+                                  val_file='val.csv',
+                                  label_file='labels.csv',
+                                  text_col='text',
+                                  label_col='label',
+                                  batch_size_per_gpu=8,
+                                  max_seq_length=512,
+                                  multi_gpu=True,
+                                  multi_label=False,
+                                  model_type='bert')
+        logger = logging.getLogger()
+        # device_cuda = torch.device("cuda")
+        device = torch.device('cuda') if torch.cuda.device_count() else torch.device('cpu')
+        metrics = [{'name': 'accuracy', 'function': accuracy}]
 
-    output_dir = Path(ROOT_DIR) / "model_output" / ""
-    output_dir.mkdir(parents=True, exist_ok=True)
-    # BertLearner可以实现训练，验证，测试
-    learner = BertLearner.from_pretrained_model(
-        databunch,
-        pretrained_path='bert-base-uncased',  # 预训练模型的位置
-        metrics=metrics,  # 希望模型在验证集上计算的度量函数的列表，如精度、beta等
-        device=device,  # cpu or gpu
-        logger=logger,  # 日志
-        output_dir=output_dir,  # 输出目录，存放trained artefacts, tokenizer vocabulary, tensorboard files
-        finetuned_wgts_path=None,  # 微调语言模型的位置
-        warmup_steps=500,
-        multi_gpu=True,
-        is_fp16=True,  # cpu上训练应该设为False
-        multi_label=False,
-        logging_steps=50)
-    learner.fit(epochs=20,
-                lr=6e-5,
-                validate=True,  # Evaluate the model after each epoch
-                schedule_type="warmup_cosine",
-                optimizer_type="lamb")  # lamb,adamw
-    learner.save_model()
+        output_dir = Path(ROOT_DIR) / ("model_output" + str(i)) / ""
+        output_dir.mkdir(parents=True, exist_ok=True)
+        # BertLearner可以实现训练，验证，测试
+        learner = BertLearner.from_pretrained_model(
+            databunch,
+            pretrained_path='bert-base-uncased',  # 预训练模型的位置
+            metrics=metrics,  # 希望模型在验证集上计算的度量函数的列表，如精度、beta等
+            device=device,  # cpu or gpu
+            logger=logger,  # 日志
+            output_dir=output_dir,  # 输出目录，存放trained artefacts, tokenizer vocabulary, tensorboard files
+            finetuned_wgts_path=None,  # 微调语言模型的位置
+            warmup_steps=500,
+            multi_gpu=True,
+            is_fp16=True,  # cpu上训练应该设为False
+            multi_label=False,
+            logging_steps=50)
+        learner.fit(epochs=20,
+                    lr=6e-5,
+                    validate=True,  # Evaluate the model after each epoch
+                    schedule_type="warmup_cosine",
+                    optimizer_type="lamb")  # lamb,adamw
+        learner.save_model()
--- text_classification_prediction.py
+++ text_classification_prediction.py
@@ -6,29 +6,31 @@ from fast_bert.prediction import BertClassificationPredictor
 from definitions import ROOT_DIR
 
 if __name__ == '__main__':
-    model_dir = ROOT_DIR + '/model_output/model_out'
-    label_dir = ROOT_DIR + '/data'
-    predictor = BertClassificationPredictor(
-        model_path=model_dir,
-        label_path=label_dir,  # location for labels.csv file
-        multi_label=False,
-        model_type='bert',
-        do_lower_case=True)
+    for i in range(0, 10):
+        model_dir = ROOT_DIR + '/model_output ' + str(i) + '/model_out'
+        label_dir = ROOT_DIR + '/data' + str(i)
+        predictor = BertClassificationPredictor(
+            model_path=model_dir,
+            label_path=label_dir,  # location for labels.csv file
+            multi_label=False,
+            model_type='bert',
+            do_lower_case=True)
 
-    test_filename = Path(ROOT_DIR) / 'data' / 'test.csv'
-    test_data_list = []
-    test_label_list = []
-    with open(test_filename, 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        line = line.replace('\n', '')
-        item_list = line.split(',')
-        annotation_type = item_list[-1]
-        sentence = ','.join(item_list[:-1])
-        test_data_list.append(sentence)
-        test_label_list.append(annotation_type)
-    multiple_predictions = predictor.predict_batch(test_data_list)
-    print(multiple_predictions)
+        test_filename = Path(ROOT_DIR) / 'data' / 'test.csv'
+        test_data_list = []
+        test_label_list = []
+        with open(test_filename, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            line = line.replace('\n', '')
+            item_list = line.split(',')
+            annotation_type = item_list[-1]
+            sentence = ','.join(item_list[:-1])
+            test_data_list.append(sentence)
+            test_label_list.append(annotation_type)
+        multiple_predictions = predictor.predict_batch(test_data_list)
+        print(multiple_predictions)
 
-    with open('predictions.json', 'w') as f:
-        json.dump(multiple_predictions, f)
+        filename = 'predictions' + str(i) + '.json'
+        with open(filename, 'w') as f:
+            json.dump(multiple_predictions, f)
