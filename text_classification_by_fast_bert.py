2c53e69c83e3784d68d1e1668fc70bdbb24407de
JScarlet <x18321758289@outlook.com>
2020-02-27 01:15:20 +0800

    添加实验代码

--- text_classification_by_fast_bert.py
+++ text_classification_by_fast_bert.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
                                   label_file='labels.csv',
                                   text_col='text',
                                   label_col='label',
-                                  batch_size_per_gpu=16,
+                                  batch_size_per_gpu=8,
                                   max_seq_length=512,
                                   multi_gpu=True,
                                   multi_label=False,
@@ -45,7 +45,7 @@ if __name__ == '__main__':
             is_fp16=True,  # cpu上训练应该设为False
             multi_label=False,
             logging_steps=50)
-        learner.fit(epochs=20,
+        learner.fit(epochs=25,
                     lr=6e-5,
                     validate=True,  # Evaluate the model after each epoch
                     schedule_type="warmup_cosine",
